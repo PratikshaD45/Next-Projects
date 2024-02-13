ca8d54763c5c2e600742a912c9561d2b0bbc4075
Pratiksha <pratikshadarekar240@gmail.com>
2024-02-13 19:21:26 +0530

    Update global styles and add Mantine styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 // import { Inter } from "next/font/google";
 import "./globals.css";
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import '@mantine/core/styles.css';
+
+import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 
 // const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +21,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
-    </html>
+    <head>
+      <ColorSchemeScript />
+    </head>
+    <body>
+      <MantineProvider>{children}</MantineProvider>
+    </body>
+  </html>
   );
 }
